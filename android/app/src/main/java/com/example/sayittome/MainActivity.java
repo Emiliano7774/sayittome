fff33f52d2603327f1d391ea98e5b272e6404cc0
Emiliano Maturano <emilianomaturano@gmail.com>
2026-05-09 11:32:59 -0300

    Fix Android MainActivity crash and update APK release

---
+++ android/app/src/main/java/com/example/sayittome/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.sayittome;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/example/sayittome/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sayittome
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
